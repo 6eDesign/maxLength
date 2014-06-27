6c589903fc273dd44b20800868dce3ed3027bbcd
Jonathan Greenemeier <jon@6eDesign.net>
2014-06-26 18:20:09 -0600

    Fixed missing HTML tag for demo (IE8 was having some issues).

--- build/js/demo.js
+++ build/js/demo.js
@@ -5,7 +5,6 @@ var demo = (function(w,d,c,$){
 		var input, markdownContainer; 
 		input = d.getElementById('markdown'); 
 		markdownContainer = d.getElementById('markdownContainer'); 
-		c.log(Markdown(input.value)); 
 		markdownContainer.innerHTML = Markdown(input.value); 
 	};
 	$(d).ready(init); 
